95de475b8e3e4c0954763118d61e9680ee49178f
nagudelo <nagudelo@payvalida.com>
2024-05-05 03:25:31 -0500

    Documentacion Swagger y Actualizacion de app.js
    Cambios en este commit:
    - Actualizacion de app.js
    - Generando events.yaml
    - Generando users.yaml
    - Generando excel.yaml
    - Generando matrices.yaml
    - Generando mapbox.yaml
    
    ¿Para qué se hace este cambio?
    - Para mejorar la organización y mantenibilidad del código al separar las responsabilidades de acceso a datos de otras partes de la aplicación
    
    Item
    - #012

--- api/src/app.js
+++ api/src/app.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
 import usersRoutes from './routes/users.router.js';
-import eventsRoutes from './routes/events.router.js'
-import excelRoutes from './routes/excel.router.js'
+import eventsRoutes from './routes/events.router.js';
+import excelRoutes from './routes/excel.router.js';
+import matricesRoutes from './routes/matrices.routes.js';
+import mapboxRoutes from './routes/mapbox.router.js'
 
 import config from './config/config.js'
 import { errorHandler } from './middleware/errorHandler.js';
@@ -10,6 +15,19 @@ import { errorHandler } from './middleware/errorHandler.js';
 const app = express();
 const port = config.port;
 
+const swaggerOptions = {
+    definition: {
+    openapi: '3.0.1',
+    info: {
+        title: 'Documentación de Plataforma para Gestion de Eventos',
+        description:
+        'Documentación que muestra todo las apis disponibles',
+    },
+    },
+    apis: [`${process.cwd()}/src/docs/**/*.yaml`],
+}
+const spec = swaggerJSDoc(swaggerOptions)
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -17,6 +35,8 @@ app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(spec))
 app.use('/api/user', usersRoutes);
 app.use('/api/events', eventsRoutes);
 app.use('/api/excel', excelRoutes);
+app.use('/api/matrices', matricesRoutes);
+app.use('/api/mapbox', mapboxRoutes);
 
 app.use(errorHandler);
 
